195af06353579ee4c31b1a0b53dbea18c23604f6
Jordi Masip <JMasip@users.noreply.github.com>
2013-11-12 08:51:21 +0100

    Delete mat.py

--- Exercici llarg matriu/mat.py
+++
@@ -1,9 +0,0 @@
-def matriuBuida(files, columnes):
-    mat = []
-    for i in range(files):
-        l=[]
-    for j in range(columnes):
-        l+=[]
-        mat+=[l]
-    return mat
-print matriuBuida
